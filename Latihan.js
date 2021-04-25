007d6d88a180e68b9cd763a53ecccd551ec2fe69
rijal2 <merentas5benua27@gmail.com>
2021-04-25 22:12:16 +0700

    Menyelesaikan tugas Ujian Array

--- Latihan.js
+++ Latihan.js
@@ -56,4 +56,24 @@ const restaurant = {
   const name = restaurant['name'];
   const favoriteDrink = restaurant['favorite drink'];
   
+
+/**  UJIAN ARRAY
+ * TODO:
+ * Buatlah sebuah variabel dengan nama evenNumber yang merupakan sebuah array dengan ketentuan:
+ *   - Array tersebut menampung bilangan genap dari 1 hingga 100
+ *
+ * Catatan:
+ *   - Agar lebih mudah, gunakanlah for loop dan logika if untuk mengisi bilangan genap pada array.
+ */
+
+// TODO
+
+const evenNumber = [];
+for (let i=1; i <= 100 ;i++) {
+  if (i % 2 == 0) {
+    evenNumber.push(i)
+  }
+}
+
+console.log(evenNumber)
   

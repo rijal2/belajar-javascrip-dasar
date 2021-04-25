9ac4e06c3ce153154ee88a88df38278f2e3cabbc
rijal2 <merentas5benua27@gmail.com>
2021-04-25 21:20:07 +0700

    Mengerjakan ujian Object

--- Latihan.js
+++ Latihan.js
@@ -18,3 +18,42 @@ let age: "24"
 console.log(firstName)
 console.log(age)
 console.log(isMale)
+
+/** UJIAN OBJECT
+ * TODO
+ * 1. Buatlah variabel dengan nama restaurant yang object dengan ketentuan berikut:
+ *    - Memiliki properti bernama "name"
+ *       - Bertipe data string
+ *       - Bernilai apa pun, asalkan tidak string kosong atau null.
+ *    - Memiliki properti bernama "city"
+ *       - Bertipe data string
+ *       - Bernilai apa pun, asalkan tidak string kosong atau null.
+ *    - Memiliki properti "favorite drink"
+ *       - Bertipe data string
+ *       - Bernilai apa pun, asalkan tidak string kosong atau null.
+ *    - Memiliki properti "favorite food"
+ *       - Bertipe data string
+ *       - Bernilai apa pun, asalkan tidak string kosong atau null.
+ *    - Memiliki properti "isVegan"
+ *       - Bertipe data boolean
+ *       - Bernilai boolean apa pun.
+ *
+ * 2. Buatlah variabel bernama name.
+ *    Kemudian isi dengan nilai name dari properti object restaurant
+ * 3. Buatlah variabel bernama favoriteDrink.
+ *    Kemudian isi dengan nilai "favorite drink" dari properti object restaurant
+ */
+
+
+// TODO
+const restaurant = {
+    name: "Cepat Saji",
+    city: "Bojonegoro",
+    "favorite drink": "Teh",
+    "favorite food": "NasKun",
+    isVegan: true,
+  }
+  const name = restaurant['name'];
+  const favoriteDrink = restaurant['favorite drink'];
+  
+  

4547bbe95c442df322216056e0f3aeced10d668a
rijal2 <merentas5benua27@gmail.com>
2021-06-17 17:18:21 +0700

    Menambahkan beberapa baris pafa arguments.js

--- 09-Argument.js
+++ 09-Argument.js
@@ -1,5 +1,5 @@
 // Cara penulian argumen
-
+// 1 . Dengan menuliskan parameter yang berada di dalam tanda kurung ().
 function jml (c, d) {
     return c + d;
 }
@@ -11,3 +11,15 @@ function dot (c, d) {
 var hasil = jml(dot(3, 4), jml(4, 3)) + dot(jml(5, 6), dot(6, 7));
 
 alert(hasil);
+
+// 2. Tidak menuliskan parameter sama sekali
+
+function colek () {
+    var hasil = 0;
+    for(var i=0; i < arguments.length; i++) {
+        hasil += arguments[i]
+    }
+    return hasil;
+}
+
+alert(colek(1,2,3,8))

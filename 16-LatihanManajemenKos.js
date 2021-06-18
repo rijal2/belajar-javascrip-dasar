c144258d786ab85093f74e2e67265c13aa145620
rijal2 <merentas5benua27@gmail.com>
2021-06-18 10:44:16 +0700

    Menambahkan file selanjutnya

---
+++ 16-LatihanManajemenKos.js
@@ -0,0 +1,36 @@
+var kamar = [];
+
+var tambahTamu = function(namaTamu, kamar){
+   // Jika kamar kosong
+    if( kamar.length == 0 ){
+        // Tambah tamu di awal array
+        kamar.push(namaTamu);
+        // kembalikan isi array & keluar dari function
+        return kamar;
+    } else {
+        // Telusuri seluruh kamar dari awal
+        for (var i = 0; i < kamar.lenght; i++){
+            // Jika ada kamar kosong
+            if(kamar[i] == undefined){
+                // Tambahkan tamu dikursi tersebut
+                kamar[i] = namaTamu;
+                // Kembalikan isi array dan keluar dari function
+                return kamar;
+            }
+            // Jika sudah ada nama yang sama
+            else if(kamar[i] == namaTamu){  
+                // Tampilkan pesan kesalahannya
+                console.log("Mohon maaf kamar kos disini mensyaratkan tidak boleh ada nama yang sama. Nama " + namaTamu + "sudah ada yang menempati")
+                // Kembalikan isi array dan keluar dari function
+                return kamar;
+            }
+            // Jika seluruh kamar sudah terisi penuh
+            else if (i == kamar.length-1 ){
+                // Tambah tamu di kamar terakhir
+                kamar.push(namaTamu);
+                // Kembalikan isi array dan keluar dari function
+                return kamar;
+            }
+        }
+    }
+}
